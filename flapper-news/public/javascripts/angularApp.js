d9da5721c8cd5ee258bcde42768fa5aa8078de11
ruben-aguilar <eagle.ruben@gmail.com>
2014-12-08 18:15:58 +0100

    Add new post to DB

--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -38,6 +38,12 @@ angular.module('flapperNews', ['ui.router'])
     });
   };
 
+  o.create = function(post) {
+    return $http.post('/posts', post).success(function(data){
+      o.posts.push(data);
+    });
+  };
+
   return o;
 }])
 
@@ -68,14 +74,9 @@ angular.module('flapperNews', ['ui.router'])
     $scope.posts = posts.posts;
     $scope.addPost = function(){
       if(!$scope.title || $scope.title === '') { return; }
-      $scope.posts.push({
+      posts.create({
         title: $scope.title,
         link: $scope.link,
-        upvotes: 0,
-        comments: [
-        {author: 'Joe', body: 'Cool post!', upvotes: 0},
-        {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-        ]
       });
       $scope.title = '';
       $scope.link = '';
